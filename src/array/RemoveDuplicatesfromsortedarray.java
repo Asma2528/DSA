5193a636bf63334325389c6db5ea0e3c7a09a277
Asma <asmait.0604@gmail.com>
2025-07-23 12:18:05 +0530

    updated code

--- src/array/RemoveDuplicatesfromsortedarray.java
+++ src/array/RemoveDuplicatesfromsortedarray.java
@@ -1,19 +1,12 @@
 // Problem: https://leetcode.com/problems/remove-duplicates-from-sorted-array
 class Solution {
     public int removeDuplicates(int[] nums) {
-        if(nums==null || nums.length==0){
-            return 0;
-        }
-
-        int k=1;
-        
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]!=nums[i-1]){
-                nums[k]=nums[i];
-                k++;
+        int i=0;
+        for(int j=0;j<nums.length;j++){
+            if(nums[i]!=nums[j]){
+                nums[++i]=nums[j];
             }
         }
-
-        return k;
+        return i+1;
     }
 }
