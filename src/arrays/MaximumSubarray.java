c6dedbca89cdf0ac096a6a3d687dc1ac456d11a7
Asma <asmait.0604@gmail.com>
2025-08-07 14:35:43 +0530

    Updated maximum subarray

--- src/arrays/MaximumSubarray.java
+++ src/arrays/MaximumSubarray.java
@@ -1,22 +1,34 @@
 // Problem: https://leetcode.com/problems/maximum-subarray/
 class Solution {
     public int maxSubArray(int[] nums) {
-        // Kadane's Algorithm
+        // Brute apporach
+        // TC - O(n^2)
+        // SC - O(1)
+        // int sum;
+        // int max = Integer.MIN_VALUE;
+        // for (int i = 0; i < nums.length; i++) {
+        //     sum = 0;
+        //     for (int j = i; j < nums.length; j++) {
+        //         sum += nums[j];
+        //         max = Math.max(max, sum);
+        //     }
+        // }
+        // return max;
+
+        // Optimal apporach (Kadane's Algorithm)
         // TC - O(n)
         // SC - O(1)
-
+        int max = nums[0];
         int sum = 0;
-        int largestSum = Integer.MIN_VALUE;
 
         for (int i = 0; i < nums.length; i++) {
             sum += nums[i];
-            largestSum = Math.max(largestSum, sum);
-
+            max=Math.max(max, sum);
             if (sum < 0) {
                 sum = 0;
             }
         }
 
-        return largestSum;
+        return max;
     }
 }
