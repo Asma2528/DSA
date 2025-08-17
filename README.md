60db4229e847e54599839c08d0de205fae40bd34
Asma <asmait.0604@gmail.com>
2025-08-17 11:13:32 +0530

    Added Search in Rotated Sorted Array

--- README.md
+++ README.md
@@ -87,3 +87,4 @@
 | 83 | Search Insert Position | [Link](https://leetcode.com/problems/search-insert-position) | Arrays | [SearchInsertPosition.java](src/arrays\SearchInsertPosition.java) |
 | 84 | Find First and Last Position of Element in Sorted Array | [Link](https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/) | Arrays | [FindFirstandLastPositionofElementinSortedArray.java](src/arrays\FindFirstandLastPositionofElementinSortedArray.java) |
 | 85 | Number of occurrence | [Link](https://www.geeksforgeeks.org/problems/number-of-occurrence2259/1) | Arrays | [Numberofoccurrence.java](src/arrays\Numberofoccurrence.java) |
+| 86 | Search in Rotated Sorted Array | [Link](https://leetcode.com/problems/search-in-rotated-sorted-array/) | Arrays | [SearchinRotatedSortedArray.java](src/arrays\SearchinRotatedSortedArray.java) |
---
+++ src/arrays/SearchinRotatedSortedArray.java
@@ -0,0 +1,42 @@
+// Problem: https://leetcode.com/problems/search-in-rotated-sorted-array/
+class Solution {
+    public int search(int[] nums, int target) {
+        // Brute force approach - Linear Search
+        // TC - O(n)
+        // SC - O(1)
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (nums[i] == target) {
+        //         return i;
+        //     }
+        // }
+        // return -1;
+
+        // Optimal approach - Binary Search (since we have to search and have a sorted array)
+        // TC - O(log n)
+        // SC - O(1)
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = low + ((high - low) / 2);
+            if (nums[mid] == target) {
+                return mid;
+            }
+            // Check if left half is sorted
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && nums[mid] >= target) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (nums[mid] <= target && nums[high] >= target) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
